704e83471360d7fe625096800d9fb74d151f29a2
Saurabh Bhardwaj <aryan.saurabhbhardwaj@gmail.com>
2025-01-21 12:46:43 +0000

    coffee machine completed

--- scripts/coffeemachine_oop.py
+++ scripts/coffeemachine_oop.py
@@ -30,19 +30,15 @@ resources = {
     "coffee": 100,
 }
 
-money = {
-    "value": 0,
-}
-
 class CoffeeMachine:
-    def __init__(self, menu, resoueces):
+    def __init__(self, menu, resources):
         self.menu = menu
-        self.resoueces = resoueces
+        self.resources = resources
         self.deposit = 0
 
     def check_available_resources(self, order):
         ingredients = self.menu[order]['ingredients']
-        if ingredients['water'] < self.resoueces['water'] and ingredients.get('milk', 0) < self.resoueces['milk'] and ingredients['coffee'] < self.resoueces['coffee']:
+        if ingredients['water'] < self.resources['water'] and ingredients.get('milk', 0) < self.resources['milk'] and ingredients['coffee'] < self.resources['coffee']:
             return True
         return False
 
@@ -51,36 +47,44 @@ class CoffeeMachine:
         dimes = int(input('Please insert the dimes: '))*0.10
         quarters = int(input('Please insert the quarters: '))*0.25
 
-        total = pennies + dimes + quarters
-        print(f"You have submitted total ${total}.")
-        return total
+        self.deposit = pennies + dimes + quarters
+        print(f"You have submitted total ${self.deposit}.")
 
 
-    def check_total_cost(total_payment, order):
-        cost = MENU[order]['cost']
-        if cost > total_payment:
-            print(f'You have not provided enough money. Please provide ${cost - total_payment} to continue.')
+    def check_total_cost(self, order):
+        cost = self.menu[order]['cost']
+        if cost > self.deposit:
+            print(f'You have not provided enough money. Please provide ${cost - self.deposit} to continue.')
             return False
         else:
             return True
 
-    def update_available_resources(order):
-        ingredients = MENU[order]['ingredients']
-        resources['water'] = resources['water'] -  ingredients['water']
-        resources['milk'] = resources['milk'] - ingredients.get('milk', 0)
-        resources['coffee'] = resources['coffee'] - ingredients['coffee']
+    def update_available_resources(self, order):
+        ingredients = self.menu[order]['ingredients']
+        self.resources['water'] = self.resources['water'] -  ingredients['water']
+        self.resources['milk'] = self.resources['milk'] - ingredients.get('milk', 0)
+        self.resources['coffee'] = self.resources['coffee'] - ingredients['coffee']
 
+    def update_resources(self):
+        water = int(input('How much water would you like to add: '))
+        milk = int(input('How much milk you wouild like to add: '))
+        coffee = int(input('How much coffee you would like to add: '))
 
-    def coffee():
+        self.resources['water'] += water
+        self.resources['milk'] += milk
+        self.resources['coffee'] += coffee
+
+    def take_order(self):
         order = input("What would you like? (espresso/latte/cappuccino): ")
-        is_resource_available = check_available_resources(order)
+        is_resource_available = self.check_available_resources(order)
         if is_resource_available:
-            total_payment = get_payment()
-            is_money_sufficient = check_total_cost(total_payment, order)
+            self.get_payment()
+            is_money_sufficient = self.check_total_cost(order)
             if is_money_sufficient:
                 print(f'Here is your {order} 🍵. Enjoy...')
-                update_available_resources(order)
-            should_continue = print('Would you like to make another order?')
+                self.update_available_resources(order)
+                
+            should_continue = input('Would you like to make another order?: ')
             if should_continue == 'yes':
                 return True
             else:
@@ -89,14 +93,29 @@ class CoffeeMachine:
             print('Sorry 😒😒, We are out of Coffee. We are on it...')
             return False
 
+    def process_order(self):
+        should_continue = True
 
-    should_continue = False
+        while should_continue:
+            while True:
+                user_input = input('What would you like to do (report, order, update): ').lower()
+                if user_input in ['report', 'order', 'update']:
+                    break
+                else:
+                    print('Please enter either Report or Order or Update.')
+            if user_input == 'report':
+                print('The current resoueces available are:\n')
+                for k, v in self.resources.items():
+                    print(f'{k}: {v}')
+            elif user_input == 'update':
+                self.update_resources()
+                print('The current resoueces available are:\n')
+                for k, v in self.resources.items():
+                    print(f'{k}: {v}')
+            else:
+                should_continue = self.take_order()
 
-    while not should_continue:
-        user_input = input('What would you like to do (report, order)').lower()
-        if user_input == 'report':
-            print('The current resoueces available are:\n')
-            for k, v in resources.items():
-                print(f'{k}: {v}')
-        else:
-            should_continue = coffee()
+
+if __name__ == "__main__":
+    coffee_machine  = CoffeeMachine(MENU, resources)
+    coffee_machine.process_order()
